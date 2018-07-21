b6f37fb796fb20ea6e1678d8d799e1151fd0b652
iNspireLeaership <otarudaudu@gmail.com>
2018-07-21 07:50:32 +0100

    creating project

---
+++ Assets/playermove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour {
+
+
+	public KeyCode moveL;
+	public KeyCode moveR;
+
+
+	public float horizvel = 0;
+
+	public float vertvel = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Rigidbody> ().velocity = new Vector3 (horizvel, 0, 4);
+
+		if (Input.GetKeyDown (moveL)) {
+
+			horizvel = -2;
+		
+		}
+	}
+
+
+
+
+
+
